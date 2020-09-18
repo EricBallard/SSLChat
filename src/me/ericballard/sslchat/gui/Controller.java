9e9f997c10209722c84d9aaac59c16d58f177788
Eric Ballard <ericballard7@gmail.com>
2020-09-17 20:56:16 -0400

    + Further developed front-end + Stylization

--- src/me/ericballard/sslchat/gui/Controller.java
+++ src/me/ericballard/sslchat/gui/Controller.java
@@ -2,8 +2,13 @@ package me.ericballard.sslchat.gui;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
+import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Circle;
 
 import java.net.URL;
@@ -15,12 +20,35 @@ public class Controller implements Initializable {
     AnchorPane anchorPane;
 
     @FXML
+    GridPane infoGrid, typingGrid, titleGrid, controlGrid;
+
+    @FXML
+    ListView listView;
+
+    @FXML
+    TextArea textArea;
+
+    @FXML
     Circle circle;
+
+    @FXML
+    ImageView soundImg;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         anchorPane.setBackground(Background.EMPTY);
         anchorPane.setClip(new Circle(circle.getLayoutX(), circle.getLayoutY(), circle.getRadius()));
+
+        titleGrid.setStyle("-fx-background-color: linear-gradient(from 25% 25% to 100% 100%, #252525, #505050)");
+
+        infoGrid.setStyle("-fx-background-color: linear-gradient(from 25% 25% to 100% 100%, #252525, #505050)");
+
+        controlGrid.setStyle("-fx-background-color: #252525");
+
+        textArea.setStyle("-fx-focus-color: transparent; -fx-text-box-border: transparent;");
+
+       //soundImg.setImage(new Image("resources/volume.png"));
+
     }
 
     public void resize() {
